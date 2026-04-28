cadb561fe74ddbe6a0ecc1400c4568f51a921a78
= <s226313877@deakin.edu.au>
2026-04-28 16:52:22 +1000

    Use cases

---
+++ use-cases/program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "This will run only on the architecture it was compiled for!\n";
+    return 0;
+}
---
+++ use-cases/program.java
@@ -0,0 +1,5 @@
+public class program {
+    public static void main(String[] args) {
+        System.out.println("This will run on any device with a JVM supporting this version of Java!");
+    }
+}
